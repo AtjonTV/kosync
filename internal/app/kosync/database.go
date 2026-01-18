b03d133877bf2f67be511ff90426417f4d1da5aa
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-01-18 21:01:01 +0100

    Extract migrations into separate file

--- internal/app/kosync/database.go
+++ internal/app/kosync/database.go
@@ -69,45 +69,6 @@ func LoadOrInitDatabase() (string, Database, error) {
 	return foundDbFile, db, nil
 }
 
-func (app *Kosync) MigrateSchema() error {
-	app.DebugPrint("[DB] Checking for Database schema migrations.")
-	latestVer := 1
-
-	migrations := map[int]interface{}{
-		1: func() {
-			for id, user := range app.Db.Users {
-				app.Db.Users[id] = UserData{
-					Username:  user.Username,
-					Password:  user.Password,
-					Documents: user.Documents,
-					History:   make(map[string]HistoryData),
-				}
-			}
-		},
-	}
-
-	if app.Db.Schema < latestVer {
-		app.DebugPrint("[DB] Migrations are available, performing backup.")
-		err := app.BackupDatabase()
-		if err != nil {
-			return err
-		}
-	} else {
-		app.DebugPrint("[DB] No Migrations to do.")
-		return nil
-	}
-
-	for ver, migrate := range migrations {
-		if app.Db.Schema < ver {
-			app.DebugPrint(fmt.Sprintf("[DB] Migrating Schema from %d to %d", app.Db.Schema, ver))
-			migrate.(func())()
-			app.Db.Schema = ver
-		}
-	}
-
-	return nil
-}
-
 func (app *Kosync) BackupDatabase() error {
 	if err := app.PersistDatabase(); err != nil {
 		return err
---
+++ internal/app/kosync/database_migration.go
@@ -0,0 +1,48 @@
+//
+// File:        internal/app/kosync/database_migration.go
+// Project:     https://git.obth.eu/atjontv/kosync
+// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+//
+
+package kosync
+
+import "fmt"
+
+func (app *Kosync) MigrateSchema() error {
+	app.DebugPrint("[DB] Checking for Database schema migrations.")
+
+	latestVer := 1
+	migrations := map[int]interface{}{
+		1: func() {
+			for id, user := range app.Db.Users {
+				app.Db.Users[id] = UserData{
+					Username:  user.Username,
+					Password:  user.Password,
+					Documents: user.Documents,
+					History:   make(map[string]HistoryData),
+				}
+			}
+		},
+	}
+
+	if app.Db.Schema < latestVer {
+		app.DebugPrint("[DB] Migrations are available, performing backup.")
+		err := app.BackupDatabase()
+		if err != nil {
+			return err
+		}
+	} else {
+		app.DebugPrint("[DB] No Migrations to do.")
+		return nil
+	}
+
+	for ver, migrate := range migrations {
+		if app.Db.Schema < ver {
+			app.DebugPrint(fmt.Sprintf("[DB] Migrating Schema from %d to %d", app.Db.Schema, ver))
+			migrate.(func())()
+			app.Db.Schema = ver
+		}
+	}
+
+	return nil
+}
