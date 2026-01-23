9a034f7d2eefda61dc3cabcb7de415c616505c3a
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-01-23 21:19:51 +0100

    Add bearer ignore as the user specified backup file is not sensistive information

--- internal/kosync/database_backup.go
+++ internal/kosync/database_backup.go
@@ -84,6 +84,8 @@ func (app *Kosync) BackupDatabase() error {
 }
 
 func RestoreDatabase(backupFile string) error {
+	// bearer:disable go_lang_log_output_neutralization
+	// bearer:disable go_lang_logger_leak
 	log.Println(fmt.Sprintf("[Restore]: Trying to restore database from file '%s'", backupFile))
 
 	_, dbFile, err := FindDatabaseFile()
