5aaa1876ba7f3dfceeaed282624de7b6e5df0b1d
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-01-25 18:33:40 +0100

    Add missing copyright headers to api files

--- internal/kosync/api_syncs.go
+++ internal/kosync/api_syncs.go
@@ -1,3 +1,9 @@
+//
+// File:        internal/kosync/api_syncs.go
+// Project:     https://git.obth.eu/atjontv/kosync
+// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+//
+
 package kosync
 
 import (
