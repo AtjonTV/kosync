48b1d7c053d6a98f7dff551286e94b585194b4ab
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-01-27 21:03:55 +0100

    webui: Recreate .keep in go generate

--- internal/webui/webui.go
+++ internal/webui/webui.go
@@ -4,6 +4,8 @@ import "embed"
 
 //go:generate bun install --cwd ../../webui
 //go:generate bun --cwd ../../webui build-only --base /web --emptyOutDir --outDir ../internal/webui/public
+// Fix .keep being deleted by "--emptyOutDir" so Git does not track it as deleted
+//go:generate touch public/.keep
 
 //go:embed public/*
 var WebUi embed.FS
