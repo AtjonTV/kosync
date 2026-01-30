c5ce42a3994a9b5430e7281496b2cd9b84f71e31
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-01-30 14:26:10 +0100

    Extract migration logic into a separate file

--- internal/kosync/database.go
+++ internal/kosync/database.go
@@ -1,9 +1,13 @@
+//
+// File:        internal/kosync/database.go
+// Project:     https://git.obth.eu/atjontv/kosync
+// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+//
+
 package kosync
 
 import (
 	"database/sql"
-	"strings"
-	"time"
 
 	_ "modernc.org/sqlite"
 )
@@ -34,102 +38,4 @@ func NewDatabase() (*Database, error) {
 
 func (db *Database) Close() error {
 	return db.rawDb.Close()
-}
-
-func (db *Database) checkAndRunMigrations() error {
-	versionsRes, err := db.rawDb.Query("SELECT version FROM schema_versions ORDER BY version DESC LIMIT 1;")
-	if err != nil && !strings.Contains(err.Error(), "no such table: schema_versions") {
-		return err
-	}
-	var currentVersion int
-	if versionsRes != nil && versionsRes.Next() {
-		if err := versionsRes.Scan(&currentVersion); err != nil {
-			return err
-		}
-	} else {
-		currentVersion = 0
-	}
-	if currentVersion < SchemaVersion {
-		db.currentSchema = currentVersion
-
-		for ver := currentVersion + 1; ver <= SchemaVersion; ver++ {
-			if err := db.migrateTo(ver); err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-func (db *Database) migrateTo(version int) error {
-	if version == 100 {
-		if _, err := db.rawDb.Exec(`
-            CREATE TABLE IF NOT EXISTS schema_versions (
-                version INTEGER PRIMARY KEY,
-                installed_at INTEGER NOT NULL
-            );
-        `); err != nil {
-			return err
-		}
-		if _, err := db.rawDb.Exec(`
-            CREATE TABLE IF NOT EXISTS users (
-                id TEXT PRIMARY KEY,
-                username TEXT UNIQE,
-                password TEXT,
-                created_at INTEGER NOT NULL,
-                updated_at INTEGER,
-                deleted_at INTEGER
-            );
-        `); err != nil {
-			return err
-		}
-		if _, err := db.rawDb.Exec(`
-            CREATE TABLE IF NOT EXISTS documents (
-                id TEXT NOT NULL,
-                owner_id TEXT NOT NULL,
-                
-                title TEXT,
-                current_location TEXT,
-                progress FLOAT,
-                last_read_on_device TEXT,
-                last_read_on_device_id TEXT,
-                last_read_at INTEGER,
-                
-                created_at INTEGER NOT NULL,
-                updated_at INTEGER,
-                deleted_at INTEGER,
-                PRIMARY KEY (id, owner_id)
-            );
-        `); err != nil {
-			return err
-		}
-		if _, err := db.rawDb.Exec(`
-            CREATE TABLE IF NOT EXISTS document_history (
-                id TEXT NOT NULL,
-                owner_id TEXT NOT NULL,
-                last_read_at INTEGER NOT NULL,
-                
-                title TEXT,
-                current_location TEXT,
-                progress FLOAT,
-                last_read_on_device TEXT,
-                last_read_on_device_id TEXT,
-                
-                created_at INTEGER NOT NULL,
-                updated_at INTEGER,
-                deleted_at INTEGER,
-                PRIMARY KEY (id, owner_id, last_read_at)
-            );
-        `); err != nil {
-			return err
-		}
-		if _, err := db.rawDb.Exec(`
-            INSERT INTO schema_versions (version, installed_at) VALUES (100, ?);
-        `, time.Now().Unix()); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
---
+++ internal/kosync/database_migration.go
@@ -0,0 +1,118 @@
+//
+// File:        internal/kosync/database_migration.go
+// Project:     https://git.obth.eu/atjontv/kosync
+// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+//
+
+package kosync
+
+import (
+	"strings"
+	"time"
+)
+
+func (db *Database) checkAndRunMigrations() error {
+	versionsRes, err := db.rawDb.Query("SELECT version FROM schema_versions ORDER BY version DESC LIMIT 1;")
+	if err != nil && !strings.Contains(err.Error(), "no such table: schema_versions") {
+		return err
+	}
+	var currentVersion int
+	if versionsRes != nil && versionsRes.Next() {
+		if err := versionsRes.Scan(&currentVersion); err != nil {
+			return err
+		}
+	} else {
+		currentVersion = 0
+	}
+	if currentVersion < SchemaVersion {
+		db.currentSchema = currentVersion
+
+		for ver := currentVersion + 1; ver <= SchemaVersion; ver++ {
+			if err := db.migrateTo(ver); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (db *Database) migrateTo(targetVersion int) error {
+	var insertSchemaVersion = `INSERT INTO schema_versions (version, installed_at) VALUES (?, ?)`
+
+	if targetVersion == 100 {
+		var createSchemaVersionTable = `
+            CREATE TABLE IF NOT EXISTS schema_versions (
+                version INTEGER PRIMARY KEY,
+                installed_at INTEGER NOT NULL
+            );
+        `
+		if _, err := db.rawDb.Exec(createSchemaVersionTable); err != nil {
+			return err
+		}
+
+		var createUsersTable = `
+            CREATE TABLE IF NOT EXISTS users (
+                id TEXT PRIMARY KEY,
+                username TEXT UNIQE,
+                password TEXT,
+                created_at INTEGER NOT NULL,
+                updated_at INTEGER,
+                deleted_at INTEGER
+            );
+        `
+		if _, err := db.rawDb.Exec(createUsersTable); err != nil {
+			return err
+		}
+
+		var createDocumentsTable = `
+            CREATE TABLE IF NOT EXISTS documents (
+                id TEXT NOT NULL,
+                owner_id TEXT NOT NULL,
+                
+                title TEXT,
+                current_location TEXT,
+                progress FLOAT,
+                last_read_on_device TEXT,
+                last_read_on_device_id TEXT,
+                last_read_at INTEGER,
+                
+                created_at INTEGER NOT NULL,
+                updated_at INTEGER,
+                deleted_at INTEGER,
+                PRIMARY KEY (id, owner_id)
+            );
+        `
+		if _, err := db.rawDb.Exec(createDocumentsTable); err != nil {
+			return err
+		}
+
+		var createDocumentHistoryTable = `
+            CREATE TABLE IF NOT EXISTS document_history (
+                id TEXT NOT NULL,
+                owner_id TEXT NOT NULL,
+                last_read_at INTEGER NOT NULL,
+                
+                title TEXT,
+                current_location TEXT,
+                progress FLOAT,
+                last_read_on_device TEXT,
+                last_read_on_device_id TEXT,
+                
+                created_at INTEGER NOT NULL,
+                updated_at INTEGER,
+                deleted_at INTEGER,
+                PRIMARY KEY (id, owner_id, last_read_at)
+            );
+        `
+		if _, err := db.rawDb.Exec(createDocumentHistoryTable); err != nil {
+			return err
+		}
+
+		if _, err := db.rawDb.Exec(insertSchemaVersion, 100, time.Now().Unix()); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
