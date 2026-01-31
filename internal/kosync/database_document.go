413023622b7d5c8ac0ce576a4a0db8f8f64665dd
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-01-31 14:47:32 +0100

    db: Implement Document create, update and get

---
+++ internal/kosync/database_document.go
@@ -0,0 +1,120 @@
+//
+// File:        internal/kosync/database_document.go
+// Project:     https://git.obth.eu/atjontv/kosync
+// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+//
+
+package kosync
+
+import (
+	"database/sql"
+	"time"
+)
+
+func (db *Database) FindDocumentById(ownerId, documentId string) (*Document, bool, error) {
+	var findOneDocument = `
+        SELECT
+            id,
+            owner_id,
+            title,
+            current_location,
+            progress,
+            last_read_on_device,
+            last_read_on_device_id,
+            last_read_at
+        FROM documents
+        WHERE id = ? and owner_id = ?;
+    `
+	rows, err := db.rawDb.Query(findOneDocument, documentId, ownerId)
+	if err != nil {
+		return nil, false, err
+	}
+	defer func(rows *sql.Rows) {
+		_ = rows.Close()
+	}(rows)
+
+	if !rows.Next() {
+		return nil, false, nil
+	}
+
+	var doc Document
+	err = rows.Scan(
+		&doc.Id,
+		&doc.OwnerId,
+		&doc.Title,
+		&doc.CurrentLocation,
+		&doc.Progress,
+		&doc.LastReadOnDevice,
+		&doc.LastReadOnDeviceId,
+		&doc.LastReadAt,
+	)
+
+	return &doc, true, err
+}
+
+func (db *Database) CreateOrUpdateDocument(doc *Document) error {
+	err := db.createHistoryEntry(doc)
+	if err != nil {
+		return err
+	}
+	var updateDocument = `
+        INSERT INTO documents (id, owner_id, title, current_location, progress, last_read_on_device, last_read_on_device_id, last_read_at, created_at)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ON CONFLICT (id, owner_id) DO
+            UPDATE SET
+                       title = ?,
+                       current_location = ?,
+                       progress = ?,
+                       last_read_on_device = ?,
+                       last_read_on_device_id = ?,
+                       last_read_at = ?,
+                       updated_at = ?;
+    `
+	now := time.Now().Unix()
+	_, err = db.rawDb.Exec(
+		updateDocument,
+		doc.Id,
+		doc.OwnerId,
+		doc.Title,
+		doc.CurrentLocation,
+		doc.Progress,
+		doc.LastReadOnDevice,
+		doc.LastReadOnDeviceId,
+		doc.LastReadAt,
+		now,
+		doc.Title,
+		doc.CurrentLocation,
+		doc.Progress,
+		doc.LastReadOnDevice,
+		doc.LastReadOnDeviceId,
+		doc.LastReadAt,
+		now,
+	)
+	return err
+}
+
+func (db *Database) createHistoryEntry(doc *Document) error {
+	_, found, err := db.FindDocumentById(doc.OwnerId, doc.Id)
+	if !found || err != nil {
+		return err
+	}
+
+	var copyToHistory = `
+        INSERT INTO document_history (id, owner_id, last_read_at, title, current_location, progress, last_read_on_device, last_read_on_device_id, created_at)
+        SELECT
+            id,
+            owner_id,
+            last_read_at,
+            title,
+            current_location,
+            progress,
+            last_read_on_device,
+            last_read_on_device_id,
+            ? as created_at
+        FROM documents
+        WHERE id = ? AND owner_id = ?;
+    `
+	now := time.Now().Unix()
+	_, err = db.rawDb.Exec(copyToHistory, now, doc.Id, doc.OwnerId)
+	return err
+}
---
+++ internal/kosync/legacy_compat.go
@@ -0,0 +1,33 @@
+package kosync
+
+import (
+	"time"
+
+	"git.obth.eu/atjontv/kosync/internal/legacy"
+)
+
+func FileDataFromNew(d *Document) legacy.FileData {
+	return legacy.FileData{
+		ProgressData: legacy.ProgressData{
+			Progress:   d.CurrentLocation,
+			Percentage: d.Progress,
+			Device:     d.LastReadOnDevice,
+			DeviceId:   d.LastReadOnDeviceId,
+		},
+		DocumentId: d.Id,
+		PrettyName: d.Title,
+		Timestamp:  d.LastReadAt,
+	}
+}
+
+func DocumentDataToNew(f *legacy.DocumentData, ownerId string) Document {
+	return Document{
+		Id:                 f.Document,
+		OwnerId:            ownerId,
+		CurrentLocation:    f.Progress,
+		Progress:           f.Percentage,
+		LastReadOnDevice:   f.Device,
+		LastReadOnDeviceId: f.DeviceId,
+		LastReadAt:         time.Now().Unix(),
+	}
+}
