721d65c5033b6c26ff3220c9479f4d2a09e50ceb
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-02-03 19:51:56 +0100

    Dev release 2026.05.0-dev.4

--- internal/kosync/kosync.go
+++ internal/kosync/kosync.go
@@ -26,7 +26,7 @@ import (
 )
 
 // Version NOTE: Must be the same as "sonar.projectVersion" in ../../sonar-project.properties
-const Version = "2026.05.0-dev.3"
+const Version = "2026.05.0-dev.4"
 
 type Kosync struct {
 	LegacyDb *legacy.LegacyDb
