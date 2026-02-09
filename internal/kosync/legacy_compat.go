1b73e46a1aad5e9b733093d256fcda5c75990599
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-02-09 14:14:53 +0100

    Add missing copyright headers

--- internal/kosync/legacy_compat.go
+++ internal/kosync/legacy_compat.go
@@ -1,3 +1,9 @@
+//
+// File:        internal/kosync/legacy_compat.go
+// Project:     https://git.obth.eu/atjontv/kosync
+// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+//
+
 package kosync
 
 import (
