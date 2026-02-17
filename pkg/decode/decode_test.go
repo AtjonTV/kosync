fda0b1c75483dc57f705b35ecc4dc43160b7cb79
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-02-17 21:15:26 +0100

    test: Tests for decode and environ packages

---
+++ pkg/decode/decode_test.go
@@ -0,0 +1,65 @@
+//
+// File:        pkg/decode/decode_test.go
+// Project:     https://git.obth.eu/atjontv/kosync
+// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+//
+
+package decode_test
+
+import (
+	"testing"
+
+	"git.obth.eu/atjontv/kosync/pkg/decode"
+)
+
+type SampleStruct struct {
+	StringField string `env:"TEST_STR" default:"TeSt"`
+	IntField    int    `env:"TEST_INT" default:"1337"`
+	BoolField   bool   `env:"TEST_BOOL" default:"true"`
+}
+
+var data = map[string]any{
+	"TEST_STR":  "Other Test",
+	"TEST_INT":  42,
+	"TEST_BOOL": false,
+}
+
+func TestStructFromMap(t *testing.T) {
+	var s = SampleStruct{}
+	err := decode.StructFromMap(&s, "env", data)
+	if err != nil {
+		t.Fatalf("Failed to decode struct: %v", err)
+	}
+
+	if s.StringField != "Other Test" {
+		t.Errorf("Expected 'Other Test', got '%s'", s.StringField)
+	}
+
+	if s.IntField != 42 {
+		t.Errorf("Expected 42, got '%d'", s.IntField)
+	}
+
+	if s.BoolField {
+		t.Errorf("Expected false, got '%t'", s.BoolField)
+	}
+}
+
+func TestStructFromEnv(t *testing.T) {
+	var s = SampleStruct{}
+	err := decode.StructFromEnv(&s)
+	if err != nil {
+		t.Fatalf("Failed to decode struct from environment: %v", err)
+	}
+
+	if s.StringField != "TeSt" {
+		t.Errorf("Expected 'TeSt', got '%s'", s.StringField)
+	}
+
+	if s.IntField != 1337 {
+		t.Errorf("Expected 1337, got '%d'", s.IntField)
+	}
+
+	if !s.BoolField {
+		t.Errorf("Expected true, got '%t'", s.BoolField)
+	}
+}
