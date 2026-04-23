994009d9622e110eac3cc14711f4439a823c9c53
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-04-23 18:11:20 +0200

    webui: Migrate from Socket API to JMP using jmp-client-js

--- webui/src/api.ts
+++ webui/src/api.ts
@@ -26,25 +26,8 @@ export async function fetchApi<T>(route: string, options: RequestInit = {}): Pro
     }
 }
 
-export async function fetchUrl<T>(route: string, options: RequestInit): Promise<{data: T | null, error: string | Response | null}> {
-  const response = await fetch(
-    `${BASE_URL}${route}`,
-    options
-  );
-  if (!response.ok) return {data: null, error: response};
-
-  if (response.headers.get('content-type')?.startsWith('application/json')) {
-    const data = await response.json() as T;
-    return {data, error: null}
-  } else {
-    return {data: await response.text() as T, error: null}
-  }
-}
-
-export async function openSocket(onConnected: (ws: WebSocket) => void, onMessage: (ws: WebSocket, message: string) => void) {
+export function getWebSocketUrl(): string|null {
   const userStore = useUserStore();
-  if (!userStore.isLoggedIn()) return;
-  const ws = new WebSocket(`${BASE_URL}/api/ws/${userStore.user.accessToken}`, ["kosync.rpc", "kosync.pubsub"]);
-  ws.onopen = (event) => onConnected(ws);
-  ws.onmessage = (event) => onMessage(ws, event.data);
+  if (!userStore.isLoggedIn()) return null;
+  return `${BASE_URL}/api/ws/${userStore.user.accessToken}`;
 }
