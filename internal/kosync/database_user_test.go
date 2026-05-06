79771518f820bf4d6ccf984cccc7eb84aa70113e
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-05-06 17:55:29 +0200

    tests: Change copyright year of new files to 2026
    These are new files and should have a copyright of the year of creation.

--- internal/kosync/database_user_test.go
+++ internal/kosync/database_user_test.go
@@ -1,7 +1,7 @@
 //
 // File:        internal/kosync/database_user_test.go
 // Project:     https://git.obth.eu/atjontv/kosync
-// Copyright:   © 2025-2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
+// Copyright:   © 2026 Thomas Obernosterer. Licensed under the EUPL-1.2 or later
 //
 
 package kosync
