2d2246c97cbaeaa9d2cb9b000ccf05ac475f52d4
Thomas Obernosterer <thomas.obernosterer@atvg-studios.com>
2026-05-16 09:39:36 +0200

    Dev release 2026.06.1-dev.11

--- internal/kosync/kosync.go
+++ internal/kosync/kosync.go
@@ -27,7 +27,7 @@ import (
 )
 
 // Version NOTE: Must be the same as "sonar.projectVersion" in ../../sonar-project.properties
-const Version = "2026.06.1-dev.10"
+const Version = "2026.06.1-dev.11"
 
 const (
 	CtxContextUserName = "current_user_name"
